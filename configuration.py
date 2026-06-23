b06402bc44916012bf7d79364d43e3fac53bf23f
AmousQiu <amousqiu@dal.ca>
2026-06-23 15:48:57 -0300

    update

--- configuration.py
+++ configuration.py
@@ -5,8 +5,8 @@ class Configuration:
     min_instruction_lines = 4
     max_instruction_lines = 64
     num_classes = 2
-    population_size = 1000
+    population_size = 25
     keep_rate = 0.8
     matrix_calc = True
     selection_method = 'normal'
-    mutation_rate = 0.5
+    mutation_rate = 0.7
